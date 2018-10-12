360989f241b68707836daff7eb5c1ebfc45c8bba
RomanBumburyak <romanbumburyak@gmail.com>
2018-10-12 14:33:31 -0400

    Two Activities working with three classes

--- AndroidResultsApp/MainActivity.cs
+++ AndroidResultsApp/MainActivity.cs
@@ -1,12 +1,15 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System.Collections.Generic;
+using Android.Content;
 
 namespace AndroidResultsApp
 {
     [Activity(Label = "AndroidResultsApp", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
+        static readonly List<string> phoneNumbers = new List<string>(); 
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -18,6 +21,7 @@ namespace AndroidResultsApp
             TextView PhoneNumberText = FindViewById<TextView>(Resource.Id.PhoneNumberText);
             TextView TranslatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneWord);
             Button TranslateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button TranslationHistoryButton = FindViewById<Button>(Resource.Id.TranslationHistoryButton);
 
             string translatedNumber = string.Empty;
 
@@ -26,14 +30,24 @@ namespace AndroidResultsApp
                 translatedNumber = Translator.ToNumber(PhoneNumberText.Text);
                 if (string.IsNullOrWhiteSpace(translatedNumber))
                 {
-                    TranslatedPhoneWord.Text = string.Empty;
+                    TranslatedPhoneWord.Text = "";
                 }
                 else
                 {
                     TranslatedPhoneWord.Text = translatedNumber;
+                    phoneNumbers.Add(translatedNumber);
+                    TranslationHistoryButton.Enabled = true;
                 }
             };
 
+
+            TranslationHistoryButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(TranslationHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
+
+            };
 
         }
     }
